adb95d90994e056a3910c393af115e1e8e98d6ee
jooaa0707 <113028667+jooaa0707@users.noreply.github.com>
2022-11-24 00:01:59 +0900

    third

--- calorie_compare.py
+++ calorie_compare.py
@@ -46,7 +46,7 @@ with col1:
     st.image('ok.jpeg')
 
 with col2:
-    cal_data = pd.read_excel('/Users/mira/Documents/GitHub/calorie9/fatsecret/fatsecret.xlsx')
+    cal_data = pd.read_excel('fatsecret.xlsx')
     cal_search=cal_data.loc[cal_data['name'].str.contains(text_input)]
     cal_search2=cal_search.sort_values(by='cal2',ascending=True)
     cal_search3=cal_search2.T
